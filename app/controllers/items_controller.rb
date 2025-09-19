e4c6cf55ccac86bd2ae24602378c0be81557aa90
todomatu <macbook.nk96@gmail.com>
2025-09-20 04:16:23 +0900

    ルーティング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,16 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :set_item, only: [:show, :edit]
+  before_action :authorize_user!, only: [:edit]
   def index
     @items = Item.order(created_at: :desc)
   end
 
   def new
     @item = Item.new
+  end
+
+  def edit
   end
 
   def create
@@ -18,10 +23,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def authorize_user
+    redirect_to root_path unless current_user == @item.user
+  end
 
   def item_params
     params.require(:item).permit(:item_name, :image, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id,
