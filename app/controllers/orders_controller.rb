17d29a322697520a01250b76c0d08cf2fcfbe7b4
todomatu <macbook.nk96@gmail.com>
2025-10-15 05:06:52 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
   before_action :move_to_root_if_not_signed_in, only: [:index, :create]
   before_action :authorize_user!, only: [:index, :create]
